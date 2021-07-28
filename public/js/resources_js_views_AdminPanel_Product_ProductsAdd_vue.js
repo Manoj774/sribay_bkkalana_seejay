580a3b8656843461da82c5a8948f0b7d077ffcf9
bkkalana <bkkalana@gmail.com>
2021-07-29 01:10:50 +0530

    update

--- public/js/resources_js_views_AdminPanel_Product_ProductsAdd_vue.js
+++ public/js/resources_js_views_AdminPanel_Product_ProductsAdd_vue.js
@@ -4490,6 +4490,22 @@ function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len 
 
       var formData = new FormData();
 
+      if (this.product.images == null) {
+        this.$toast.open({
+          message: "Please add images to this product...!",
+          type: 'error'
+        });
+        return;
+      }
+
+      if (this.product.product_categories == null) {
+        this.$toast.open({
+          message: "Please add category to this product...!",
+          type: 'error'
+        });
+        return;
+      }
+
       for (var i = 0; i < this.product.images.length; i++) {
         var file = this.product.images[i];
         formData.append('images[' + i + ']', file);
